022817d7ad39dabaae3f3b2e5ca4b4c39ff210f9
Pichayawat Karnjanaves <pichayawat_k@flowaccount.com>
2021-07-14 20:01:32 +0700

    add vertical chip groups that similar to email apps

--- app/src/main/java/com/souringhosh/materialchipapplication/MainActivity.kt
+++ app/src/main/java/com/souringhosh/materialchipapplication/MainActivity.kt
@@ -6,6 +6,7 @@ import android.text.TextWatcher
 import android.view.KeyEvent
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.chip.Chip
+import com.google.android.material.textfield.TextInputEditText
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,7 +18,7 @@ class MainActivity : AppCompatActivity() {
         textInputEditText.background = null
 
         /*
-        * Listening to text changes in the TextInputEditext field and generate new chip on entering a comma */
+        * Listening to text changes in the TextInputEdittext field and generate new chip on entering a comma */
         textInputEditText.addTextChangedListener(object: TextWatcher {
 
             override fun afterTextChanged(s: Editable?) {
@@ -42,7 +43,7 @@ class MainActivity : AppCompatActivity() {
         })
 
         /*
-        * Deleting chips on pressing backspace on kepad*/
+        * Deleting chips on pressing backspace on keypad*/
         textInputEditText.setOnKeyListener { _, _, event ->
             if (event != null && event.action == KeyEvent.ACTION_DOWN && event.keyCode == KeyEvent.KEYCODE_DEL) {
                 if (textInputEditText.length() == 0 && chipGroup.childCount > 0) {
@@ -53,6 +54,36 @@ class MainActivity : AppCompatActivity() {
             false
         }
 
+        setEmailTextAction()
+    }
 
+    private fun setEmailTextAction() {
+        emailTextInputEditText.background = null
+
+        /*
+        * Deleting chips on pressing backspace on keypad*/
+        emailTextInputEditText.setOnKeyListener { view, keyCode, event ->
+            if (event != null && event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_DEL) {
+                if ((view as TextInputEditText).length() == 0 && emailChipGroup.childCount > 0) {
+                    val chip = emailChipGroup.getChildAt(emailChipGroup.childCount - 1) as Chip
+                    emailChipGroup.removeView(chip)
+                }
+            }
+            if (event != null && event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
+                val s = emailTextInputEditText.text
+                val trimmed = s.toString().trim { it <= ' ' }
+                if (trimmed.length > 1) {
+                    val chip = Chip(this@MainActivity)
+                    chip.text = trimmed.substring(0, trimmed.length)
+                    chip.isCloseIconVisible = true
+                    chip.setOnCloseIconClickListener {
+                        emailChipGroup.removeView(chip)
+                    }
+                    emailChipGroup.addView(chip)
+                    s?.clear()
+                }
+            }
+            false
+        }
     }
 }
